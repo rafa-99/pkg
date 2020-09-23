17b5e82c5392cbd9d38e6efa44945d304d28e6e5
rafa_99 <rafa99@protonmail.com>
2020-09-23 13:50:07 +0000

    Fixed Segmentation Fault resulted from no args

--- pkg.c
+++ pkg.c
@@ -5,12 +5,20 @@
  **********************************************************
  * All in One Package Manager for easier "Distro Hopping" *
  **********************************************************
-*/
+ */
 
 int main(int argc, char **argv)
 {
-	char* arg = stringedArgument(argc, argv);
-	actions(*(argv[1]), arg);
-	free(arg);
+	if ( argc > 1 )
+	{
+		char* arg = stringedArgument(argc, argv);
+		actions(*(argv[1]), arg);
+		free(arg);
+	}
+	else
+	{
+		actions('h', "");
+	}
+
 	return 0;
 }
