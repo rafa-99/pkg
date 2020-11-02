06d4de9cfb6596f636f060410d4481623043dfe7
rafa_99 <rafa99@protonmail.com>
2020-11-02 19:37:41 +0000

    Fixed Memory Allocation size per package

--- utils.c
+++ utils.c
@@ -68,7 +68,7 @@ int detectOsType()
 char* stringedArgument(int argc, char** argv)
 {
 	int numberOfTotalPackages = argc - 2;
-	char* arg = (char *) calloc(((numberOfTotalPackages * 80) + numberOfTotalPackages), sizeof(char));
+	char* arg = (char *) calloc((numberOfTotalPackages * 80), sizeof(char));
 
 	for( int i = 2; i < argc; i++ )
 	{
